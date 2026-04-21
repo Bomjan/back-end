60ac36d593797ec1508a022e3c7902ec53274eac
Bomjan <sundrabomjan@gmail.com>
2026-04-21 15:13:09 +0600

    Course: Completed the CRUD operations

--- Backend_Basics/02_practical/myapp/views/js/course.js
+++ Backend_Basics/02_practical/myapp/views/js/course.js
@@ -5,7 +5,12 @@ const getFormData = () => {
     courseName: $("cname").value,
   };
 };
+const resetForm = () => {
+  document.getElementById("cid").value = "";
+  document.getElementById("cname").value = "";
+};
 
+window.onload = getCourse();
 function addCourse() {
   let data = getFormData();
 
@@ -16,8 +21,96 @@ function addCourse() {
   }).then((res) => {
     if (res.ok) {
       console.log("Course Added sucessfully");
+      showCourse(data);
     } else {
       console.log("Some errors in adding the course", res);
+    }
+
+    resetForm();
+  });
+}
+
+function getCourse() {
+  fetch("/course")
+    .then((res) => res.text())
+    .then((data) => JSON.parse(data))
+    .then((obj) => obj.forEach((c) => showCourse(c)));
+}
+
+console.log("Logging the response from /course");
+// getCourse();
+
+function showCourse(course) {
+  let table = $("myTable");
+  // table.innerHTML = "";
+  row = table.insertRow(table.length);
+  td = [];
+
+  for (let i = 0; i < table.rows[0].cells.length; i++) {
+    td[i] = row.insertCell(i);
+  }
+
+  td[0].innerHTML = course.courseId;
+  td[1].innerHTML = course.courseName;
+  td[2].innerHTML = `<input type="button" onclick="deleteCourse(this)" value="delete" id="button-1">`;
+  td[3].innerHTML = `<input type="button" onclick="updateCourse(this)" value="update" id="button-add">`;
+}
+
+function deleteCourse(r) {
+  const cid = r.parentElement.parentElement.cells[0].textContent;
+  fetch("/course/" + cid, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
+  }).then((res) => {
+    if (res.ok) {
+      // also remove from the table
+      resetTable();
+      getCourse();
+    }
+  });
+}
+function updateCourse(r) {
+  const selectedRow = r.parentElement.parentElement;
+  const cid = selectedRow.cells[0].textContent;
+  const oldData = {
+    courseId: parseInt(selectedRow.cells[0].textContent),
+    courseName: selectedRow.cells[1].textContent,
+  };
+  updateForm(cid, oldData);
+}
+
+const resetTable = () =>
+  ($("myTable").innerHTML = `<tr>
+      <th>Course ID</th>
+      <th>Course Name</th>
+      <th></th>
+      <th></th>
+  </tr>`);
+
+function updateForm(cid, oldData) {
+  const button = $("button-add");
+  button.textContent = "Update";
+  button.setAttribute("onclick", `update(${cid})`);
+  document.getElementById("cid").value = oldData.courseId;
+  document.getElementById("cname").value = oldData.courseName;
+}
+
+function update(cid) {
+  const newData = getFormData();
+  fetch("/course/" + cid, {
+    method: "PUT",
+    body: JSON.stringify(newData),
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
+  }).then((res) => {
+    if (res.ok) {
+      console.log("Course updated successfully");
+      resetTable();
+      getCourse();
+      resetForm();
+      $("button-add").textContent = "Add";
+      $("button-add").setAttribute("onclick", "addCourse()");
+    } else {
+      alert("Error updating course");
     }
   });
 }
